86203e3457a3ceac87d138b55e1931f27690764e
Sean <seanchaney462@u.boisestate.edu>
2024-09-18 19:18:09 -0600

    Implement notepad-like style feature

--- org/gjt/sp/jedit/EditPane.java
+++ org/gjt/sp/jedit/EditPane.java
@@ -992,6 +992,8 @@ public class EditPane extends JPanel implements BufferSetListener
 			jEdit.getColorProperty("view.eolMarkerColor"));
 		painter.setWrapGuidePainted(jEdit.getBooleanProperty(
 			"view.wrapGuide"));
+		painter.setNotepadEnabled(jEdit.getBooleanProperty(
+				"view.enableNotepadAppearance"));
 		painter.setWrapGuideColor(
 			jEdit.getColorProperty("view.wrapGuideColor"));
 		painter.setCaretColor(
